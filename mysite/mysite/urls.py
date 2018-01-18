2e8626c53863d8f1d2174c7ced8069409cd6f0da
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-01-17 22:45:34 -0200

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
